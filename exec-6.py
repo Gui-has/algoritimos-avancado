848fda501b894091240818ec3191296a0dcebe13
Gui-has <guiesteves30@gmail.com>
2024-12-11 19:38:22 -0300

    Atualizar o exec-6.py

--- exec-6.py
+++ exec-6.py
@@ -1,4 +1,4 @@
-#escreva um algoritimo para ler as dimensõesde um retângulo (base e altura), calcular e escrever a area do retângulo
+#escreva um algoritimo para ler as dimensões de um retângulo (base e altura), calcular e escrever a area do retângulo
 
 altura= int(input("digite alutra do retangulo"))
 base= int(input("digite base do retangulo"))
